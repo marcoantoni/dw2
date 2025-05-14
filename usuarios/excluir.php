7bd9c7f92c5315d85abf78e11a0bf1c2d93b6cb0
Marco Antoni <marco.antoni910@gmail.com>
2025-05-13 21:04:57 -0300

    CRUD: delete
    verificação dos registros excluídos

--- usuarios/excluir.php
+++ usuarios/excluir.php
@@ -9,10 +9,15 @@
 
 	if (mysqli_query($conn, $sql) ){
 		// consulta exececutada com sucesso
-		session_start();
-		$_SESSION["msg_sucesso"] = "Usuário excluído com sucesso";
-		header("location: mostrar.php");
+		// mysqli_affected_rows conta quantas linhas foram afetadas na última consulta executada. É importante essa verificação, pois pode acontecer de ser usado um id que não existe, de modo que a consulta vai ser executada com sucesso, mas não quer dizer que houve a exclusão de um registro
+		if (mysqli_affected_rows($conn) == 1){
+			session_start();
+			$_SESSION["msg_sucesso"] = "Usuário excluído com sucesso";
+		}
 	} else {
 		echo ("Houve um erro ao excluir o registro");
 	}
+	
+	header("location: mostrar.php");
+
 ?>
