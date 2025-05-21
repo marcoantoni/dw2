e6c8f557fbcd39a1af12b972353a5a2530f1e829
Marco Antoni <marco.antoni910@gmail.com>
2025-05-20 21:18:57 -0300

    Base de dados usada nas aulas

--- dw2.sql
+++ dw2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16/04/2025 às 01:30
+-- Tempo de geração: 21/05/2025 às 02:18
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `escolaridades`
+--
+
+CREATE TABLE `escolaridades` (
+  `id` int(11) NOT NULL,
+  `escolaridade` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `escolaridades`
+--
+
+INSERT INTO `escolaridades` (`id`, `escolaridade`) VALUES
+(1, 'Ensino fundamental'),
+(2, 'Ensino médio');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `usuarios`
 --
 
@@ -33,21 +52,29 @@ CREATE TABLE `usuarios` (
   `nascimento` date NOT NULL,
   `email` varchar(45) NOT NULL,
   `tipo` int(11) NOT NULL,
-  `senha` varchar(128) NOT NULL
+  `senha` varchar(128) NOT NULL,
+  `id_escolaridade` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `nascimento`, `email`, `tipo`, `senha`) VALUES
-(1, 'Jonas', '0000-00-00', 'jonas@gmail.com', 1, '1234'),
-(2, 'Silvio', '0000-00-00', 'silvio@gmail.com', 1, '1234'),
-(3, 'Juliana', '2003-01-01', 'juliana@gmail.com', 2, '12345678');
+INSERT INTO `usuarios` (`id`, `nome`, `nascimento`, `email`, `tipo`, `senha`, `id_escolaridade`) VALUES
+(14, 'Jim Kurose', '1956-01-01', 'kurose@network.com', 2, 'aula', 0),
+(15, 'Paulo Henrique da Costa', '1995-04-21', 'paulo@ymail.com', 1, '1234', 2),
+(16, 'rayane', '0000-00-00', 'raymeloc@gmail.com', 2, '******', 2),
+(17, 'rayane', '2001-07-19', 'raymeloc@gmail.com', 2, '******', 2);
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `escolaridades`
+--
+ALTER TABLE `escolaridades`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `usuarios`
@@ -60,10 +87,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `escolaridades`
+--
+ALTER TABLE `escolaridades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
