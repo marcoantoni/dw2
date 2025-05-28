aa09188fe7e3c91ffedbe1c14bb8f9105fbee95f
Marco Antoni <marco.antoni910@gmail.com>
2025-05-27 21:21:12 -0300

    Protegendo o código contra SQL Injection

--- usuarios/editar.php
+++ usuarios/editar.php
@@ -9,30 +9,49 @@
 <body>
     <div class="container">
         <?php
-            $id_usuario = $_GET["id"]; // pegando o parametro de usuário que vem da url
+            $id_usuario = isset($_GET["id"]) ? (int) $_GET["id"] : 0; // pegando o parametro de usuário que vem da url e faz a conversão para int
 
-            $conn = mysqli_connect("localhost", "root", "", "dw2");
+            // abrindo uma conexão de banco de dados usando o mysqli
+            $conn = new mysqli("localhost", "root", "", "dw2");
 
-            if ($conn) {
+            // testa se conectou com o banco de dados
+            if ($conn->connect_error) {
+                die ("Houve um erro ao conectar com o banco de dados");
+            }            
 
-                // string da consulta responsável por recuperar o registro a ser editado
-               $sql = "SELECT * FROM usuarios WHERE id = $id_usuario";
+            // string da consulta responsável por recuperar o registro a ser editado
+            // o ? será o parametro que será inserido na consulta
+            $sql = "SELECT * FROM usuarios WHERE id = ?";
 
-               $resultado = mysqli_query($conn, $sql);
+            // definindo e preparando a consulta parametrizada
+            $stmt = $conn->prepare($sql);    
 
-               if (mysqli_num_rows( $resultado) == 1) {
-                    // encontrou o registro 
-                    $usuario = mysqli_fetch_array($resultado);
+            // definindo os paramatros da consulta (cada ?) 
+            // i - int
+            // f - float
+            // s - string
+            $stmt->bind_param("i", $id_usuario); 
 
-                    $nome = $usuario["nome"];
-                    $nasc = $usuario["nascimento"];
-                    $email = $usuario["email"];
-                    $tipo = $usuario["tipo"];
-               } else {
-                    // não encontrou nenhum registro
-                    header("location: mostrar.php");
-               }
+            $stmt->execute();    // executando a consulta
+
+            $resultado = $stmt->get_result();   // armazenando o resultado da consulta no result set
+
+            //testando quantas linhas o result set retornou
+            if ($resultado->num_rows == 1) {
+                // encontrou o registro 
+                $usuario = $resultado->fetch_assoc();
+
+                $nome = $usuario["nome"];
+                $nasc = $usuario["nascimento"];
+                $email = $usuario["email"];
+                $tipo = $usuario["tipo"];
+            } else {
+                // não encontrou nenhum registro
+                header("location: mostrar.php");
             }
+            
+            $stmt->close(); // encerra a consulta
+            $conn->close(); // fecha a conexão com o banco de dados
         ?>
         <h2>Editando usuário</h2>
         <form method="POST" action="proc_cad.php">
